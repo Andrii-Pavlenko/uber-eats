2363813baf9fe382f0007685fc58fc3e8e329568
Andrii <farikpite@gmail.com>
2019-11-30 14:44:46 +0200

    Updates

---
+++ precache-manifest.78ea1527573e94b6459663588856f347.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6679d63e72fa84fa167f63dcd50369",
+    "url": "/uber-eats/index.html"
+  },
+  {
+    "revision": "804d85b5f6264c352cc5",
+    "url": "/uber-eats/static/css/main.89786716.chunk.css"
+  },
+  {
+    "revision": "61e63a0fc74d8b4d2a7c",
+    "url": "/uber-eats/static/js/2.f6be08f8.chunk.js"
+  },
+  {
+    "revision": "804d85b5f6264c352cc5",
+    "url": "/uber-eats/static/js/main.6e6a1364.chunk.js"
+  },
+  {
+    "revision": "4087a64efece2967b512",
+    "url": "/uber-eats/static/js/runtime~main.17ba1c32.js"
+  }
+]);
